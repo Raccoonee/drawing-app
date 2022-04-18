e0116983c3c6ecf565f05b3e02ce0ff66119096d
Raccoonee <93836398+Raccoonee@users.noreply.github.com>
2022-04-18 18:27:18 -0500

    style: fix footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <p>
         <a href="https://github.com/Raccoonee">GitHub</a>
       </p>
-      <h2>Created by Raccoonee</h2>
+      <h2 className="header2">Created by Raccoonee</h2>
       <p>
         <a href="https://www.youtube.com/channel/UCIwswzNBnEwyWFRNBZ_lbsw">
           Youtube
