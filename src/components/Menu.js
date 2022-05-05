e801669ba166039149790097e922524438f9b830
Raccoonee <93836398+Raccoonee@users.noreply.github.com>
2022-05-05 18:52:47 -0500

    Feat: add functionality to save drawing button

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,29 @@
 /* eslint-disable no-unused-vars */
 import ClearCanvasButtom from "./ClearCanvasButton";
-
+import { saveAs } from "file-saver";
 import React, { useRef, useContext, useState } from "react";
 import { Button } from "@mui/material";
 import "./menu.css";
 import { useCanvas } from "./CanvasContext";
 
 const Menu = () => {
-  const { canvasRef, prepareCanvas, startDrawing, finishDrawing, draw, colors } =
-    useCanvas();
- 
+  const {
+    canvasRef,
+    prepareCanvas,
+    startDrawing,
+    finishDrawing,
+    draw,
+    colors,
+  } = useCanvas();
 
+  
 
+  const downloadImage = () => {
+    const canvas = document.getElementsByTagName("canvas");
+    const drawingURL = canvas[0].toDataURL('image/png');
+    saveAs(drawingURL, 'drawing.jpg')
+  };
+  
 
   return (
     <>
@@ -20,8 +32,10 @@ const Menu = () => {
           <div>
             <ClearCanvasButtom />
           </div>
-          <div className="save-button">
-            <Button variant="contained" onClick={() => {alert('Not added yet!')}}>Save Drawing</Button>
+          <div>
+            <Button variant="contained" onClick={downloadImage}  href="">
+              Save Drawing
+            </Button>
           </div>
         </div>
         <div className="c">
@@ -30,19 +44,34 @@ const Menu = () => {
             <button onClick={colors.red} class="color-buttons red"></button>
             <button onClick={colors.blue} class="color-buttons blue"></button>
             <button onClick={colors.cyan} class="color-buttons cyan"></button>
-            <button onClick={colors.orange} class="color-buttons orange"></button>
-            <button onClick={colors.maroon} class="color-buttons maroon"></button>
+            <button
+              onClick={colors.orange}
+              class="color-buttons orange"
+            ></button>
+            <button
+              onClick={colors.maroon}
+              class="color-buttons maroon"
+            ></button>
             <button onClick={colors.green} class="color-buttons green"></button>
             <button onClick={colors.teal} class="color-buttons teal"></button>
           </div>
           <div>
             <button onClick={colors.white} class="color-buttons white"></button>
             <button onClick={colors.lime} class="color-buttons lime"></button>
-            <button onClick={colors.yellow} class="color-buttons yellow"></button>
-            <button onClick={colors.magenta} class="color-buttons magenta"></button>
+            <button
+              onClick={colors.yellow}
+              class="color-buttons yellow"
+            ></button>
+            <button
+              onClick={colors.magenta}
+              class="color-buttons magenta"
+            ></button>
             <button onClick={colors.gray} class="color-buttons gray"></button>
             <button onClick={colors.olive} class="color-buttons olive"></button>
-            <button onClick={colors.purple} class="color-buttons purple"></button>
+            <button
+              onClick={colors.purple}
+              class="color-buttons purple"
+            ></button>
             <button onClick={colors.navy} class="color-buttons navy"></button>
           </div>
         </div>
